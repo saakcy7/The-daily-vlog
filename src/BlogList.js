7ed8f0a8b77f908251153d2792660d52eb4eddef
Sakshi KC <kcsakshi92@gmail.com>
2024-07-04 17:00:26 +0545

    fetched data from db.json file

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,5 +1,5 @@
 
-const BlogList=({blogs,title,handleDelete})=>{
+const BlogList=({blogs,title})=>{
    
   
     
@@ -11,7 +11,7 @@ const BlogList=({blogs,title,handleDelete})=>{
             <div className="blog-preview" key={blog.id}>
             <h2>{blog.title}</h2>
             <p>Written by {blog.author}</p>
-            <button onClick={()=>handleDelete(blog.id)}>Delete Blog</button>
+
         </div>
     ))}
     </div>
