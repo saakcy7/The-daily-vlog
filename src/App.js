8d832ead92535a682e71c8645c6223dfc1b9279d
Sakshi KC <kcsakshi92@gmail.com>
2024-07-04 22:23:22 +0545

    routing done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import Navbar from './Navbar';
 import Home from './Home'
+import Create from './Create'
+import { BrowserRouter as Router,Route, Routes} from 'react-router-dom';
 function App() {
   return (
+    <Router>
     <div className="App">
        <Navbar/>
       <div className="content">
-        <Home/>
+      <Routes>
+      <Route exact path="/" element={<Home/>}></Route>
+      <Route path="/create" element={<Create/>}></Route>
+  
+    
+      </Routes>
       </div>
     </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,8 @@
 import { useState,useEffect } from 'react';
 import BlogList from './BlogList';
-
+import useFetch from './useFetch';
 const Home = () => {
-    const [blogs, setBlogs]=useState([]);
-      const [isPending, setIsPending]=useState(true);
-    const [error, setError]=useState(null);
-    
-  
-    useEffect(()=>{
-      setTimeout(()=>{
-        fetch('http://localhost:8000/blogs')
-        .then(res=>{
-       if(!res.ok){
-           throw Error('Could not fetch the data for that resource');
-        }
-        return res.json();
-      })
-        .then(data=>{
-            console.log(data);
-            setBlogs(data);
-            setIsPending(false);
-            setError(null);
-        })
-        .catch(err=>{
-            setError(err.message);
-            setIsPending(false);
-        })
-      },1000);
-    },[]);
+  const {data:blogs,isPending,error}=useFetch('http://localhost:8000/blogs');
     return ( 
         <div className="home">
           {error && <div>{error}</div>}
