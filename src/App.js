cbf6ddcce64c30c5d4b3b0c00b0906981c913ce8
Sakshi KC <kcsakshi92@gmail.com>
2024-07-04 23:23:22 +0545

    404 error for routes that donot exist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home'
 import Create from './Create'
+import NotFound from './NotFound';
 import BlogDetails from './BlogDetails'
 import { BrowserRouter as Router,Route, Routes} from 'react-router-dom';
 function App() {
@@ -13,7 +14,7 @@ function App() {
       <Route exact path="/" element={<Home/>}></Route>
       <Route path="/create" element={<Create/>}></Route>
       <Route path="/blogs/:id" element={<BlogDetails/>}></Route>
-  
+      <Route path="*" element={<NotFound/>}></Route>
     
       </Routes>
       </div>
