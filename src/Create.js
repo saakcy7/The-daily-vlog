9f7e9a6df95ebbd504c05af91877b5daf58a2c7d
Sakshi KC <kcsakshi92@gmail.com>
2024-07-04 22:59:58 +0545

    created a form for new blogs

--- src/Create.js
+++ src/Create.js
@@ -1,8 +1,39 @@
 import React from 'react'
-
+import {useState} from 'react';
 function Create (){
+    const [title,setTitle]=useState('');
+    const [body,setBody]=useState('');
+    const [author,setAuthor]=useState('mario');
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        const blog={title,body,author};
+        console.log(blog);
+    }
   return (
-    <div>Create</div>
+    <div className='create'>
+    <h2>Add a New Blog</h2>
+    <form onSubmit={handleSubmit}>
+        <label>Blog Title:</label>
+        <input
+            type='text'
+            required value={title}
+            onChange={(e)=>setTitle(e.target.value)}
+        />
+        <label>Blog Body:</label>
+        <textarea
+            required value={body}
+            onChange={(e)=>setBody(e.target.value)}
+        ></textarea>
+        <label>Blog Author:</label>
+        <select value={author} onChange={(e)=>setAuthor(e.target.value)}>
+            <option value='mario'>mario</option>
+            <option value='yoshi'>yoshi</option>
+        </select>
+        <button>Add Blog</button>
+    
+    </form>
+    </div>
   )
 }
 export default Create;
